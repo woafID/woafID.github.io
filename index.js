17a325dae59fbd21396d4ef43357c921c2add33f
woafID <xmmv@pm.me>
2024-06-09 04:22:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-function downloadFile() {
-    const url = 'https://raw.githubusercontent.com/woafID/psychic-engine/main/Affinity_Linux.sh';
-    const filename = 'Affinity_Linux.sh';
-  
-    fetch(url)
-      .then(response => response.blob())
-      .then(blob => {
-        const link = document.createElement('a');
-        link.href = URL.createObjectURL(blob);
-        link.download = filename;
-        link.click();
-      });
-  }
+function downloadFile(url, element) {
+  fetch(url)
+    .then(response => response.blob())
+    .then(blob => {
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(blob);
+      
+      const filename = element.getAttribute('filename');
+      
+      link.download = filename;
+      link.click();
+    });
+}
