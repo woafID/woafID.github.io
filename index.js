a8096083d782b87a8b7cb99679d8feb1534f268f
woafID <xmmv@pm.me>
2026-05-12 21:28:09 +0200

    Refresh Page Look

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 function downloadFile(url, element) {
-  fetch(url)
-    .then(response => response.blob())
-    .then(blob => {
-      const link = document.createElement('a');
-      link.href = URL.createObjectURL(blob);
-      
-      const filename = element.getAttribute('filename');
-      
-      link.download = filename;
-      link.click();
-    });
+  const filename = element.getAttribute('filename');
+
+  showNotification(filename);
+
+  setTimeout(() => {
+    fetch(url)
+      .then(response => response.blob())
+      .then(blob => {
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = filename;
+        link.click();
+      });
+  }, 5000);
+}
+
+function showNotification(filename) {
+  const modal = document.getElementById('download-modal');
+  const filenameSpans = document.querySelectorAll('.filename-placeholder');
+
+  filenameSpans.forEach(span => span.textContent = filename);
+
+  modal.classList.add('active');
+}
+
+function closeNotification() {
+  const modal = document.getElementById('download-modal');
+  modal.classList.remove('active');
 }
